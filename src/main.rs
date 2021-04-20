c96e4daaa0c1bfa832b391fd4d84c5c9e226b400
Tom Streller <tom@streller.me>
2021-04-20 09:49:34 +0200

    Bevy stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::{prelude::*, render::camera::CameraProjection};
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(WindowDescriptor {
+            title: "Space Conflict!".to_string(),
+            width: 1280.,
+            height: 720.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        ..Default::default()
+    });
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-7.0, 20.0, 4.0),
+        )),
+        ..Default::default()
+    });
+    // Light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
 }
